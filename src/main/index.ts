2231cef41f7b90327b31f4aca0b36653aab807b0
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-11-12 20:40:37 -0300

    chore: setting the application window size

--- src/main/index.ts
+++ src/main/index.ts
@@ -7,7 +7,7 @@ function createWindow(): void {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 900,
-    height: 900,
+    height: 600,
     show: true,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
