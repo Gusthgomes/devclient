70e339adc319cc6909058fd7cf25da7c54d4f82a
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-11-12 20:28:08 -0300

    chore: initial project setup

--- src/main/index.ts
+++ src/main/index.ts
@@ -7,8 +7,8 @@ function createWindow(): void {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 900,
-    height: 670,
-    show: false,
+    height: 900,
+    show: true,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
@@ -55,8 +55,6 @@ app.whenReady().then(() => {
   createWindow()
 
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
