7d4b9c4f680a030d3554b1c7fdf57bb466cd354e
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-11-13 20:57:06 -0300

    feat: creating routes

---
+++ src/renderer/src/Routes.tsx
@@ -0,0 +1,23 @@
+import { Router, Route } from 'electron-router-dom'
+
+import { About } from './pages/about'
+import { Create } from './pages/create'
+import { Detail } from './pages/detail'
+import { Home } from './pages/homes'
+
+import { Layout } from './components/layout'
+
+export function Routes() {
+  return (
+    <Router
+      main={
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/detail" element={<Detail />} />
+        </Route>
+      }
+    />
+  )
+}
