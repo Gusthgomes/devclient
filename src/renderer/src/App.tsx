a8e0775ad4fb7f3852198eb2016c3b138e76b7b4
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-11-13 21:00:09 -0300

    feat: applying routes

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,5 @@
+import { Routes } from './Routes'
+
 export default function App() {
-  return (
-    <div className="flex px-3 py-5 gap-3 font-semibold">
-      <h1 className="text-green-500 text-2xl">Meu primeiro projeto</h1>
-    </div>
-  )
+  return <Routes />
 }
