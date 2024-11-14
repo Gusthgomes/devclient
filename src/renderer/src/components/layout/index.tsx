5343b7b6a4475c4f7f387e1cd28910c0daba6ba9
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-11-13 21:06:46 -0300

    feat: initial Header component

--- src/renderer/src/components/layout/index.tsx
+++ src/renderer/src/components/layout/index.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from 'react-router-dom'
+import { Header } from '../Header'
 
 export function Layout() {
   return (
     <div className="flex-1 flex flex-col max-h-screen">
-      <h1>LAYOUT</h1>
+      <Header />
       <Outlet />
     </div>
   )
