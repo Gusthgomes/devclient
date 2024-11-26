07cae9837ce96cf8008cb9371d5cecf994ace7e6
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-11-25 21:10:13 -0300

    chore: add keyFrames and animations

--- src/renderer/tailwind.config.js
+++ src/renderer/tailwind.config.js
@@ -5,6 +5,20 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: 'Poppins, sans-serif'
+      },
+      keyframes: {
+        slideIn: {
+          from: { width: 0 },
+          to: { width: 'var(--radix-collapsible-content-width)' }
+        },
+        slideOut: {
+          from: { width: 'var(--radix-collapsible-content-width)' },
+          to: { width: 0 }
+        }
+      },
+      animation: {
+        slideIn: 'slideIn 0.28s',
+        slideOut: 'slideOut 0.28s'
       }
     }
   },
