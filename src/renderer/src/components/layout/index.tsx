83025681176367cf32a26b35811cc11650eac327
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-11-25 21:12:19 -0300

    feat: add Sidebar and Header components inside Layout component

--- src/renderer/src/components/layout/index.tsx
+++ src/renderer/src/components/layout/index.tsx
@@ -1,11 +1,24 @@
 import { Outlet } from 'react-router-dom'
 import { Header } from '../Header'
 
+import * as Collapsible from '@radix-ui/react-collapsible'
+import { Sidebar } from '../Sidebar'
+
+import { useState } from 'react'
+
 export function Layout() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true)
   return (
-    <div className="flex-1 flex flex-col max-h-screen">
-      <Header />
-      <Outlet />
-    </div>
+    <Collapsible.Root
+      defaultOpen
+      className="h-screen w-screen bg-gray-950 text-slate-100 flex"
+      onOpenChange={setIsSidebarOpen}
+    >
+      <Sidebar />
+      <div className="flex-1 flex flex-col max-h-screen">
+        <Header isSidebarOpen={isSidebarOpen} />
+        <Outlet />
+      </div>
+    </Collapsible.Root>
   )
 }
