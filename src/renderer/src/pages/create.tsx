f520537265402dff83f6cc8965dd73a3bb8e51e4
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-11-27 20:25:36 -0300

    feat: add button to registering a new customer

--- src/renderer/src/pages/create.tsx
+++ src/renderer/src/pages/create.tsx
@@ -1,7 +1,22 @@
 export function Create() {
+  async function handleAddCustomer() {
+    const doc = {
+      name: 'Gustavo Gomes',
+      email: 'Gusth@mail.com',
+      phone: '999131556',
+      address: 'Rua das flores, 123',
+      role: 'developer',
+      status: true
+    }
+
+    const response = await window.api.addCustomer(doc)
+    console.log(response)
+  }
   return (
     <div>
       <h1>Página create</h1>
+
+      <button onClick={handleAddCustomer}>Cadastrar cliente</button>
     </div>
   )
 }
