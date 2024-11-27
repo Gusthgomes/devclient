e67396598c11f8081d8bbc17bbc32cd1174d6668
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-11-26 21:03:53 -0300

    feat: consuming data from the new channel

--- src/renderer/src/pages/homes.tsx
+++ src/renderer/src/pages/homes.tsx
@@ -1,6 +1,10 @@
 import { Link } from 'react-router-dom'
 
 export function Home() {
+  async function handleAdd() {
+    const response = await window.api.fetchUsers()
+    console.log(response)
+  }
   return (
     <div>
       <h1>Página principal</h1>
@@ -8,6 +12,10 @@ export function Home() {
       <Link to="/create" className="bg-blue-600 rounded-md p-2 flex gap-3 w-52">
         Ir para a página create
       </Link>
+
+      <br></br>
+
+      <button onClick={handleAdd}>Buscar usuários</button>
     </div>
   )
 }
